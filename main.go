7febe72b02f7ee5a23d1fdc22dce4f6628a8a96b
Crownss <jesenkun@gmail.com>
2022-07-20 20:29:36 +0700

    fixing_1

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/crownss/dark_market/config"
@@ -15,6 +16,10 @@ func main(){
 		fmt.Println("Cannot load environment")
 	}
 	config.InitDB()
+	log.Println("\n\n\t\t\tRemember !\n\tyou're not requiered to fill RUN_HOST and RUN_PORT in .env file\n\t the default running on http://localhost:8000\n\t")
 	e := routers.Router()
-	e.Run(":" + os.Getenv("PORT"))
+	if os.Getenv("RUN_HOST")!=""&&os.Getenv("RUN_PORT")!=""{
+		e.Run(os.Getenv("RUN_HOST")+":"+os.Getenv("RUN_PORT"))
+	}
+	e.Run("localhost:8000")
 }
